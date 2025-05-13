dc860a4576a30cbb174fb7eb9377e1480dbbce5e
Eiji Ensing <=>
2025-05-13 21:31:11 +0200

    working addi

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,4 @@
-use crate::inst::{MASK_ADDI, MATCH_ADDI};
+use crate::inst::{MASK_ADDI, MASK_LB, MATCH_ADDI, MATCH_LB};
 
 #[derive(Default)]
 pub struct VM {
@@ -30,10 +30,29 @@ impl VM {
         let bytes_instruction = u32::from_le_bytes(bytes.try_into().unwrap());
 
         if bytes_instruction & MASK_ADDI == MATCH_ADDI {
-            println!("ADDI INST");
+            self.handle_i_instruction(bytes_instruction, |_, a, b| a.wrapping_add(b));
+        } else if bytes_instruction & MASK_LB == MATCH_LB {
+            self.handle_i_instruction(bytes_instruction, |vm, a, b| {
+                let addr = a.wrapping_add(b) as usize;
+                let byte = vm.memory[addr]; // load 1 byte
+                (byte as i8) as i32
+            });
         }
 
         0
+    }
+
+    fn handle_i_instruction<F>(&mut self, bytes_instruction: u32, operation: F)
+    where
+        F: FnOnce(&mut Self, i32, i32) -> i32,
+    {
+        let rd = ((bytes_instruction >> 7) & 0x1f) as usize;
+        let rs1 = ((bytes_instruction >> 15) & 0x1f) as usize;
+        let imm = (bytes_instruction as i32) >> 20;
+
+        let rs1_val = self.registers[rs1];
+        let result = operation(self, rs1_val, imm);
+        self.registers[rd] = result;
     }
 }
 
@@ -48,8 +67,13 @@ mod tests {
     }
 
     #[test]
-    fn test_adfds() {
-        let mut vm = VM::new(0, vec![0xC, 0x8, 0, 0, 0, 0, 0, 0]);
+    fn test_addi() {
+        // ADDI is a I type instruction
+        // imm          rs1   f3  rd    opcode
+        // 000000000101 00000 000 00000 0010011
+        // 00000000 01010000 00000000 00010011
+        // 0x13 0x0 0x50 0x0
+        let mut vm = VM::new(0, vec![0x13, 0x0, 0x50, 0x0]);
         vm.run();
         panic!("hi");
     }
